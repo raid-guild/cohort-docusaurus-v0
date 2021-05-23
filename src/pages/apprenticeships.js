1f426a38af8f3b8f482291e5111b4e2cc988ee72
jonathanprozzi <jonathanprozzi@gmail.com>
2021-05-23 16:53:42 -0400

    added apprenticeships page

---
+++ src/pages/apprenticeships.js
@@ -0,0 +1,37 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+
+function ApprenticePageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>Apprenticeships</p>
+      </div>
+    </header>
+  );
+}
+
+export default function ApprenticeshipPage() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Raid Guild ${siteConfig.title} Apprenticeships`}
+      description='Raid Guild apprenticeships in Season 1'
+    >
+      <ApprenticePageHeader />
+      <main>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores vitae
+        provident reiciendis expedita est suscipit illum culpa labore quae.
+        Tempore dolorem nam architecto obcaecati doloremque! Natus amet dolor
+        aperiam facere?
+      </main>
+    </Layout>
+  );
+}
