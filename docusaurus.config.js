801a6e99b16547fd3e9a4a1e4a949453d486f51b
jonathanprozzi <jonathanprozzi@gmail.com>
2021-05-23 16:58:33 -0400

    added /projects and /apprenticeships to navbar via docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,79 +1,82 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Season 1',
-  tagline: 'Slaying Web3 Product Demons',
-  url: 'https://bafybeieqwggeehqd6croo6mwafxc75zpeaq4h2vrzylkoitrtoqo6pyjai.ipfs.infura-ipfs.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Raid Guild', // Usually your GitHub org/user name.
-  projectName: 'raid-guild-cohort-docusaurus-v0', // Usually your repo name.
-  plugins: ['@crunchypie/neanderthals'],
+  title: "Season 1",
+  tagline: "Slaying Web3 Product Demons",
+  url: "https://bafybeieqwggeehqd6croo6mwafxc75zpeaq4h2vrzylkoitrtoqo6pyjai.ipfs.infura-ipfs.io/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Raid Guild", // Usually your GitHub org/user name.
+  projectName: "raid-guild-cohort-docusaurus-v0", // Usually your repo name.
+  plugins: ["@crunchypie/neanderthals"],
   themeConfig: {
     neanderthals: {
-      'orbit-cid': ''
+      "orbit-cid": "",
     },
     navbar: {
-      title: 'Season 1',
+      title: "Season 1",
       logo: {
-        alt: 'Crossed Swords',
-        src: 'img/logo.svg',
+        alt: "Crossed Swords",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Tutorial",
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        { to: "/blog", label: "Blog", position: "left" },
+        { to: "/projects", label: "Projects", position: "left" },
+        { to: "/apprenticeships", label: "Apprenticeships", position: "left" },
+
         {
-          href: 'https://github.com/orgs/raid-guild/',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/orgs/raid-guild/",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Medium',
-              href: 'https://medium.com/raid-guild',
+              label: "Medium",
+              href: "https://medium.com/raid-guild",
             },
             {
-              label: 'Discord',
-              href: 'https://discord.com/invite/rGFpfQf',
+              label: "Discord",
+              href: "https://discord.com/invite/rGFpfQf",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/RaidGuild',
+              label: "Twitter",
+              href: "https://twitter.com/RaidGuild",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/orgs/raid-guild/',
+              label: "GitHub",
+              href: "https://github.com/orgs/raid-guild/",
             },
           ],
         },
@@ -83,22 +86,22 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
