1ecf5be413061fbb0a30dcac38375b827f5a81bc
jonathanprozzi <jonathanprozzi@gmail.com>
2021-05-24 09:55:19 -0400

    adding styles for roles - now side by side

--- src/components/ApprenticeCard.js
+++ src/components/ApprenticeCard.js
@@ -2,12 +2,19 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./ApprenticeCard.module.css";
 
-const ApprenticeCard = ({ name, roles, site, twitter }) => {
+const ApprenticeCard = ({ name, site, twitter }) => {
+  const techRoles = ["Warrior", "Paladin"];
+  const nonTechRoles = ["Scribe", "Healer"];
   return (
     <div className={styles.apprenticeBox}>
       <h3 className={styles.apprenticeName}>Name</h3>
-      <p>Warrior, Paladin</p>
-      <p>Scribe, Healer</p>
+      <div style={{ display: "flex" }}>
+        {techRoles
+          ? techRoles.map((role) => (
+              <span className={styles.apprenticeRole}>{role}, </span>
+            ))
+          : null}
+      </div>
     </div>
   );
 };
