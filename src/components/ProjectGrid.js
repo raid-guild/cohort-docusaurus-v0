1ab9d6a5ff361277a9a573c1ecfbd089ac090fdc
jonathanprozzi <jonathanprozzi@gmail.com>
2021-05-25 14:54:22 -0400

    added keys to the grids (oops) -- using just a name + map index until we pull from a db with an id

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -6,7 +6,9 @@ const ProjectGrid = ({ projectData }) => {
   return (
     <div className={styles.projectGrid}>
       {projectData &&
-        projectData.map((project) => <ProjectCard name={project.name} />)}
+        projectData.map((project, idx) => (
+          <ProjectCard key={`${project.name}${idx}`} name={project.name} />
+        ))}
     </div>
   );
 };
