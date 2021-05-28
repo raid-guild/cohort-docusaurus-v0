849c5f6fe3a448919e11e216d55bb10e61cb5319
jonathanprozzi <jonathanprozzi@gmail.com>
2021-05-27 22:37:26 -0400

    created a sorted prop and function for apprentice data on ApprenticeGrid

--- src/components/ApprenticeGrid.js
+++ src/components/ApprenticeGrid.js
@@ -3,11 +3,19 @@ import clsx from "clsx";
 import styles from "./ApprenticeGrid.module.css";
 import ApprenticeCard from "./ApprenticeCard";
 
-const ApprenticeGrid = ({ apprenticeData }) => {
+const ApprenticeGrid = ({ apprenticeData, sorted }) => {
+  const sortedData =
+    sorted === "alphabetical"
+      ? apprenticeData.sort((apprenticeOne, apprenticeTwo) =>
+          apprenticeOne.name > apprenticeTwo.name ? 1 : -1
+        )
+      : apprenticeData;
+
+  console.log("sorted", sortedData);
   return (
     <div className={styles.apprenticeGrid}>
       {apprenticeData &&
-        apprenticeData.map((apprentice, idx) => (
+        sortedData.map((apprentice, idx) => (
           <ApprenticeCard
             key={`${apprentice.name}${idx}`}
             name={apprentice.name}
